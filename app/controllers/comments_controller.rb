bcf1b7d88ffb6db080d80e85a5db3fcc842a8ee2
P Maryon <maryonpawel@gmail.com>
2022-05-24 11:53:45 +0100

    comments destroy action implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :comment_find_article
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create!(comment_params)
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: 303
+  end
   private
-
+  def comment_find_article
+    @article = Article.find(params[:article_id])
+  end
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
